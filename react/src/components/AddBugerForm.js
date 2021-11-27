b42f5e04debf6edc02a44a748852ce7e2d3c39bd
KaterynaTrushko <trushkokateryna@gmail.com>
2021-11-27 20:37:41 +0200

    button order_avail_unavail

--- react/src/components/AddBugerForm.js
+++ react/src/components/AddBugerForm.js
@@ -12,7 +12,7 @@ class AddBurgerForm extends React.Component {
     event.preventDefault();
     const burger = {
       name: this.nameRef.current.value,
-      price: parseFloat(this.priceRef.current.value),
+      price: parseFloat(this.priceRef.current.value || 0),
       status: this.statusRef.current.value,
       desc: this.descRef.current.value,
       image: this.imageRef.current.value,
--- react/src/components/Burger.js
+++ react/src/components/Burger.js
@@ -3,7 +3,7 @@ import React from "react";
 class Burger extends React.Component {
   render() {
     const { image, name, desc, status, price } = this.props.details;
-
+    const isAvalilable = status === "available";
     return <li className="menu-burger">
       <div className="image-container">
         <img src={image} alte={name} />
@@ -13,7 +13,7 @@ class Burger extends React.Component {
           <span className="price">{price}$</span>
         </h3>
         <p>{desc}</p>
-        <button className="buttenOrder">Order</button>
+        <button className="buttenOrder" disabled={!isAvalilable}>{isAvalilable ? "Order" : "out of stock"}</button>
       </div>
     </li>
 
